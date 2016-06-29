be840cf1b1291749444db63ae55e7cbf81d77394
Rafal Wilinski <raf.wilinski@gmail.com>
2016-06-29 23:06:41 +0200

    Pipeline fully functional

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 'use strict';
 const http = require('axios');
+const AWS = require('aws-sdk');
+
 const path = 'static/';
+const bucketName = 'rwilinski.me';
+
+const s3 = new AWS.S3({
+  params: {
+    Bucket: bucketName
+  }
+});
+
+const putFileToS3 = (fileObject) => new Promise((resolve, reject) => {
+  http.get(fileObject.download_url)
+  .then((payload) => {
+    s3.putObject({
+      Bucket: bucketName,
+      Key: fileObject.name,
+      Body: payload.data
+    }, (error, data) => {
+      if (error) {
+        return reject(error);
+      } else {
+        return resolve(data);
+      }
+    });
+  })
+  .catch((error) => {
+    return reject(error);
+  });
+});
 
 exports.handler = (event, context, callback) => {
     const downloadsUrl = JSON.parse(event.Records[0].Sns.Message).repository.contents_url.replace('{+path}', path);
+    let processed = 0;
+
+    const updateProgress = (totalCount) => {
+      processed++;
+      console.log(`Progress: ${processed} out of ${totalCount}`);
+      if (processed === totalCount) {
+        callback(null, 'Done!');
+      }
+    }
+
     http.get(downloadsUrl, {
       headers: {
-        'User-Agent': 'AWS Lambda Function'
+        'User-Agent': 'AWS Lambda Function' // Without that Github will reject all requests
       }
     })
     .then((payload) => {
-      callback(null, 'SIEMANKO ;]');
+      payload.data.forEach((fileObject) => {
+        putFileToS3(fileObject)
+        .then(() => updateProgress(payload.data.length))
+        .catch((error) => callback(error, `Error while uploading ${fileObject.name} file to S3`));
+      });
     })
     .catch((error) => {
       callback(error, 'Failed to get files in repository.');
