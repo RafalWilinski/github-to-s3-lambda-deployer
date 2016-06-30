1a784fe56b4f4bfb35c3de5d2504d4394703b855
Rafal Wilinski <raf.wilinski@gmail.com>
2016-06-30 10:16:34 +0200

    Added mimetypes generation

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk');
 
 const path = 'static/';
 const bucketName = process.env.BUCKET_NAME;
-const storageClass = 'REDUCED_REDUNDANCY'; // STANDARD | REDUCED_REDUNDANCY | STANDARD_IA
+const storageClass = 'STANDARD'; // STANDARD | REDUCED_REDUNDANCY | STANDARD_IA
 
 const confirmationTopicArn = process.env.AWS_CONFIRMATION_SNS_TOPIC_ANR;
 const mailMessage = `Deploy to ${bucketName}/${path} succeeded!`;
@@ -16,6 +16,20 @@ const s3 = new AWS.S3({
     Bucket: bucketName
   }
 });
+
+const computeContentType = (filename) => {
+  const parts = filename.split('.');
+  switch (filename.split('.')[parts.length-1]) {
+    case 'png':
+      return "image/png";
+    case 'html':
+      return "text/html";
+    case 'js':
+      return "application/javascript";
+    case 'css':
+      return "text/css";
+  }
+};
 
 const confirmUpload = (callback) => {
   sns.publish({
@@ -39,6 +53,8 @@ const putFileToS3 = (fileObject) => new Promise((resolve, reject) => {
   .then((payload) => {
     s3.putObject({
       Bucket: bucketName,
+      ACL: 'public-read',
+      ContentType: computeContentType(fileObject.name),
       Key: fileObject.name,
       Body: payload.data,
       StorageClass: storageClass
